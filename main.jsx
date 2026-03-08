20a114f8c98cf5bbbd58b446a4aaec2677f882e3
nil000nilesh <nil000nilesh@gmail.com>
2026-03-08 18:58:58 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ChessTrainingApp from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChessTrainingApp />
+  </React.StrictMode>,
+)
